f3a217184a44f5c2c2b1f422fb4e67ef17c778c1
Omar Rios <omarrx14@gmail.com>
2024-10-25 03:51:47 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,17 +16,18 @@ def run_migrations():
 async def startup_event():
     run_migrations()
 
+
+app.include_router(user.router, prefix="/api/users", tags=["users"])
+app.include_router(project.router, prefix="/api/projects", tags=["projects"])
+
 app.add_middleware(
     CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
     allow_origins=[
         "https://music-app-frontend-pc8g.vercel.app",
         "https://music-app-frontend-pc8g-5x5jpup4y-omars-projects-b5a3697e.vercel.app",
         "https://music-gen-demo-omars-projects-b5a3697e.vercel.app"
     ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
-
-app.include_router(user.router, prefix="/api/users", tags=["users"])
-app.include_router(project.router, prefix="/api/projects", tags=["projects"])
