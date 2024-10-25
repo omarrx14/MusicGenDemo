5bdc21518b68e322285e916525218e7a23cf0894
Omar Rios <omarrx14@gmail.com>
2024-10-24 20:21:50 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,28 +6,19 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # Permite solo tu origen de desarrollo
-    allowed_origins=[
-        "http://localhost:5173",
+    allow_origins=[
+        # URL de producción del frontend en Vercel
         "https://music-app-frontend-pc8g.vercel.app",
-        "https://music-gen-demo.vercel.app",
-        "https://music-app-frontend-pc8g-ciautrm86-omars-projects-b5a3697e.vercel.app",
-        "https://music-gen-demo-omars-projects-b5a3697e.vercel.app"
-    ]
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=allowed_origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    app.include_router(user.router, prefix="/api/users", tags=["users"])
-    app.include_router(
-        project.router, prefix="/api/projects", tags=["projects"])
+app.include_router(user.router, prefix="/api/users", tags=["users"])
+app.include_router(project.router, prefix="/api/projects", tags=["projects"])
 
 
-@ app.get("/")
+@app.get("/")
 async def root():
     return {"message": "Welcome to the music app API!"}
