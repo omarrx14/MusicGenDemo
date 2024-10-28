05bfb67beaf6619ae89e3e7613bb67a4bd8cf9fa
Omar Rios <omarrx14@gmail.com>
2024-10-27 20:15:20 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ from alembic.config import Config
 from alembic import command
 from fastapi import FastAPI
 from routers import user, project
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+
+# from starlette.middleware.cors import CORSMiddleware
+
 
 # Añadir la ruta de la raíz del proyecto para permitir importaciones relativas
 
@@ -12,19 +15,29 @@ os.environ["DATABASE_URL"] = "postgres://default:MbpU67rvGqKg@ep-green-credit-a4
 
 app = FastAPI()
 
+
+app.include_router(user.router, prefix="/api/users", tags=["users"])
+app.include_router(project.router, prefix="/api/projects", tags=["projects"])
+
+app.add_middleware(
+    CORSMiddleware,
+    # Permite cualquier subdominio de Vercel
+    allow_origin_regex="https://.*\.vercel\.app",
+    allow_credentials=True,
+    allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["*"],
+)
+
+
 origins = [
     "https://music-app-frontend-pc8g.vercel.app",
     "https://music-gen-demo-omars-projects-b5a3697e.vercel.app"
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["Authorization", "Content-Type"],
-)
-
-
-app.include_router(user.router, prefix="/api/users", tags=["users"])
-app.include_router(project.router, prefix="/api/projects", tags=["projects"])
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["Authorization", "Content-Type"],
+# )
